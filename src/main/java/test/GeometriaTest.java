3e091f4f31ce0f47c8906b6b0bd8f14bf437d72b
sansung14 <78426816+sansung14@users.noreply.github.com>
2021-03-03 21:20:56 +0100

    UD21

---
+++ src/main/java/test/GeometriaTest.java
@@ -0,0 +1,79 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import JUnit.Junit09_Geometria.dto.Geometria;
+
+class GeometriaTest {
+
+	@Test
+	public void testareacuadrado() {
+		int resultado = Geometria.areacuadrado(2);
+		int esperado = 4;
+		assertEquals(esperado, resultado);
+	}
+
+	@Test
+	public void testareaCirculo() {
+		double resultado = Geometria.areaCirculo(2);
+		double esperado = 12.5664;
+		assertEquals(esperado, resultado);
+		
+	}
+	
+	@Test
+	public void testareaTriangulo() {
+		int resultado = Geometria.areatriangulo(2,2);
+		int esperado = 2;
+		assertEquals(esperado, resultado);
+	}
+
+	@Test
+	public void testareaRectangulo() {
+		int resultado = Geometria.arearectangulo(2,2);
+		int esperado = 4;
+		assertEquals(esperado, resultado);
+	}
+	
+	@Test
+	public void testareaPentagono() {
+		int resultado = Geometria.areapentagono(2,2);
+		int esperado = 2;
+		assertEquals(esperado, resultado);
+	}
+	
+
+	@Test
+	public void testareaRombo() {
+		int resultado = Geometria.arearombo(2,2);
+		int esperado = 2;
+		assertEquals(esperado, resultado);
+	}
+	
+	@Test
+	public void testareaRomboide() {
+		int resultado = Geometria.arearomboide(2,2);
+		int esperado = 4;
+		assertEquals(esperado, resultado);
+	}
+
+	
+	@Test
+	public void testareaTrapecio() {
+		int resultado = Geometria.areatrapecio(2,2,2);
+		int esperado = 4;
+		assertEquals(esperado, resultado);
+	}
+	
+	@Test
+	public void testFiguras() {
+		String resultado = Geometria.figura(1);
+		String esperado = "cuadrado";
+		assertEquals(esperado, resultado);
+	}
+	
+	
+
+}
